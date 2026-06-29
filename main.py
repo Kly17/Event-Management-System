1dc7380c822d46dff40aedd653ae06e267c9c7a7
Kly17 <ivankly17@gmail.com>
2026-06-29 19:01:37 +0800

    Adjusted view function for OOP

--- main.py
+++ main.py
@@ -6,16 +6,21 @@ while True:
 
     print("\n===== EVENT MANAGEMENT SYSTEM =====")
     print("1. Add Event")
-    print("2. Exit")
-
+    print("2. View Event")
+    print("3. Exit")
+    
     choice = input("Enter your choice: ")
 
     if choice == "1":
         manager.add_event()
-
+    
     elif choice == "2":
+        manager.view_events()
+
+    elif choice == "3":
         print("Exiting...")
         break
 
     else:
         print("Invalid choice.")
+        
